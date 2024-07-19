d28e3f8a8619baece54ce50976676da70e957e30
DreameR <aab8117@gmail.com>
2024-07-19 23:12:41 +0300

    ...

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class Hueta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
+
+
+/*
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello mazafaka",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontSize: 30,
+              color: const Color.fromARGB(255, 121, 2, 2),
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+  );
+}
+
+ */
+
+/* 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          title: Text("Нихуя не понятно"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text(
+            "Но скоро будет все понятно",
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+        
+      ),
+    ),
+  );
+} */
